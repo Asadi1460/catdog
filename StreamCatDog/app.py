68ae408978589c492dc407ce602417610dd9a8a4
Your Name <you@example.com>
2023-11-20 22:51:03 +0330

    revise2

--- StreamCatDog/app.py
+++ StreamCatDog/app.py
@@ -7,34 +7,20 @@ import numpy as np
 from preprocess import *
 import streamlit as st
 # import emoji
-
 #from bs4 import BeautifulSoup
 #from urllib.request import urlopen
 
-# Set a Unicode font that supports Farsi characters
-st.markdown(
-    """
-    <style>
-        body {
-            font-family: 'Noto Sans', sans-serif;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+#@st.cache_data
+@st.cache
 
-
-@st.cache_data
 def process_image(file_buffer):
     # Convert the file buffer to a NumPy array
     image = np.frombuffer(file_buffer.getvalue(), dtype=np.uint8)
     # Decode the image
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)  # Use 0 for grayscale
-
     # Your image processing logic using OpenCV
     # For example, convert to grayscale
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
     return image
 
 # Constants
@@ -43,7 +29,6 @@ img_shape = (img_size[0], img_size[1], 3)
 
 # Load the model
 model = tf.keras.models.load_model("ResNet.h5")
-
 
 def main():
     st.title("Classify CAT & Dog")
@@ -57,13 +42,9 @@ def main():
         
         # Display the original image        
         st.image(cv_image, caption="Uploaded Image", use_column_width='auto')
-
         resized_image = cv2.resize(cv_image, (224, 224))
-
         preprocessed_image = cascaded_preprocessing(resized_image)
-
         prediction = model.predict(np.expand_dims(preprocessed_image, axis=0))
-
         predicted_class = 'dog' if prediction[0, 0] > 0.5 else 'cat'
         st.info(f'Predicted Class: {predicted_class}')
 
