980d5d0cc0a1024637f82f243db9e249d7963f3a
Asadi1460 <eng.asadi@gmail.com>
2023-11-24 20:41:48 +0330

    cv2->pillow

--- app1.py
+++ app1.py
@@ -1,40 +1,92 @@
-import altair as alt
+from PIL import Image
+import tensorflow as tf
 import numpy as np
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+def preprocess_images(images):
+    preprocessed_images = []
+    for i in range(len(images)):
+        image = images[i]
+        
+        # Convert the RGB image to LAB color space
+        img = Image.fromarray(image, 'RGB')
+        
+        # Apply median filter to the LAB image
+        img = img.filter(ImageFilter.MedianFilter(size=5))
+        
+        # Split the LAB image into L, A, and B channels
+        r, g, b = img.split()
+    
+        # Apply CLAHE to each channel separately
+        clahe = ImageEnhance.Contrast(r).enhance(6.0)
+        r = clahe
+        clahe = ImageEnhance.Contrast(g).enhance(6.0)
+        g = clahe
+        clahe = ImageEnhance.Contrast(b).enhance(6.0)
+        b = clahe
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+        # Merge the enhanced RGB channels 
+        img_output = Image.merge('RGB', (r, g, b))
+       
+        # Convert the LAB image back to RGB color space
+        preprocessed_image = np.array(img_output)
+        
+        preprocessed_images.append(preprocessed_image)
+    
+    return np.array(preprocessed_images)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def cascaded_preprocessing(image):
+    # Convert the RGB image to LAB color space
+    img = Image.fromarray(image, 'RGB')
+    
+    # Apply additional preprocessing to the output of preprocess_images
+    preprocessed_image = preprocess_images([img])
+    
+    return preprocessed_image[0]
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+@st.cache_data
+def process_image(file_buffer):
+    # Convert the file buffer to a NumPy array
+    image = np.array(Image.open(file_buffer))
+    # Your image processing logic using Pillow
+    # For example, convert to RGB
+    image = Image.fromarray(image, 'RGB')
+    return np.array(image)
+    
+# imshow
+def imshow(cv_image, caption="Uploaded Image", use_column_width='auto'):
+    st.image(cv_image, caption=caption, use_column_width=use_column_width)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+# predict
+def predict_image(cv_image):
+    resized_image = Image.fromarray(cv_image).resize((224, 224))
+    preprocessed_image = cascaded_preprocessing(np.array(resized_image))
+    prediction = model.predict(np.expand_dims(preprocessed_image, axis=0))
+    predicted_class = 'dog' if prediction[0, 0] > 0.5 else 'cat'
+    st.info(f'Predicted Class: {predicted_class}')
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# Constants
+img_size = (224, 224)
+img_shape = (img_size[0], img_size[1], 3)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+# Load the model
+# model = tf.keras.models.load_model("ResNet.h5")
+model = tf.keras.models.load_model('ResNet.h5')
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+def main():
+    st.title("Classify CAT & Dog")
+
+    # File uploader
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+		# Process the image
+        cv_image = process_image(uploaded_file)
+        
+        # Display the original image        
+        imshow(cv_image)
+        # predict the image
+        predict_image(cv_image)
+
+if __name__ == "__main__":
+    main()
